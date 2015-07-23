66b6f609f687ee96442e46212c00f5f81ffedc45
Mauro Ribeiro <mauro0x52@gmail.com>
2015-07-23 13:15:36 -0400

    Version 0.1

---
+++ Algorithms/Algorithm.js
@@ -0,0 +1,2 @@
+var SCA = require('sca.js');
+var Algorithm = new SCA.Composite('Algorithm', __dirname + '/composites/model-'+process.argv[2]+'.json');
---
+++ Modeler/Modeler.js
@@ -0,0 +1,7 @@
+var SCA = require('sca.js');
+var fs = require('fs');
+
+var file = fs.readFileSync('./composite.json', 'utf8');
+fs.writeFileSync('./composite.json.tmp', file);
+
+var DeployerComposite = new SCA.Composite('DeployerComposite', './composite.json.tmp');
